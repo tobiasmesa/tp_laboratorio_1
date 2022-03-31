5c3f46cd563c808a4de3035722241bdce276efbd
tobiasmesa <tobiasmesa030303@gmail.com>
2022-03-31 08:09:27 -0300

    Probando

---
+++ prueba1/src/prueba1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : prueba1.c
+ Author      : tobiasmesa
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
