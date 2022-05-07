14c250b440fa6d88c4ef105beda076b6d2b5dced
tobiasmesa <tobiasmesa030303@gmail.com>
2022-05-07 15:12:28 -0300

    Agregando TP2

---
+++ TP_2/src/TP_2.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : TP_2.c
+ Author      : Tobias Mesa
+ Version     :
+ Copyright   : TP2 Laboratorio 1
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+int main(void) {
+	puts("TP2"); /* prints TO2 */
+	return EXIT_SUCCESS;
+}
